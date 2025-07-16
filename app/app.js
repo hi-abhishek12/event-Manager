451e58c995ce350feef028586808e3d9deeed520
hi-abhishek12 <abhisahni2005@gmail.com>
2025-07-16 11:10:42 +0530

    event manager

--- app/app.js
+++ app/app.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mysql = require("mysql2/promise");
+const path = require('path');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get("/", async (req, res) => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "eventManager",
+  });
+  
+  let result = await connection.query('SELECT * FROM eventManager.appUsers')
+  connection.end();
+
+  res.render('index',{data : result[0]})
+});
 
 app.listen(port, () => {
-  console.log(`Example app running on port ${port}`)
-})
+  console.log(`app running on port ${port}`);
+});
