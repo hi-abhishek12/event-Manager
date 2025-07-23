34f954f8fc5ae5a2f5b2795084f4a6aa98e289fc
hi-abhishek12 <abhisahni2005@gmail.com>
2025-07-23 21:47:09 +0530

    event book

--- app/app.js
+++ app/app.js
@@ -10,12 +10,14 @@ app.set("view engine", "ejs");
 // Serve Static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
+app.use('/bootstrap-icons', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
+
+
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 
 // Routing
-
 const route = require('./routes/webRoute')
 app.use('/',route);
 
