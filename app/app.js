04a7aa7d6861b03573cf9d39490c358759397906
hi-abhishek12 <abhisahni2005@gmail.com>
2025-07-30 17:41:02 +0530

    event booking

--- app/app.js
+++ app/app.js
@@ -10,44 +10,52 @@ const dbconfig = require('../app/config/dbconfig')
 app.set("view engine", "ejs");
 
 // Serve Static files
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use('/bootstrap-icons', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
 
 
-// middleware
+// middlewares
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-// app.set('trust proxy', 1);
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true, maxAge: 3600000 },
-//   rolling: true
-// }))
+
 const sessionStore = new MySQLStore(dbconfig);
 app.use(
   session({
-    key: "connect.sid",           // cookie name
-    secret: "mySecretKey123",     // keep it strong & secure
-    store: sessionStore,          // here we pass the store
+    key: "connect.sid",         
+    secret: "mySecretKey123",     
+    store: sessionStore,          
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 2 * 3600000,        // 2 hours
+      maxAge: 2 * 3600000,       
       httpOnly: true,
-      secure: false               // keep false for localhost
+      secure: false               
     }
   })
 );
 
+// CRUD OPERATIONS
+
+// app.put("/api/cards/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { title } = req.body;
+//   const card = cards.find(c => c.id == id);
+//   if (!card) return res.status(404).json({ error: "Not found" });
+//   card.title = title;
+//   res.json(card);
+// });
+
 
 // Routing
+
 const route = require('./routes/webRoute')
 app.use('/',route);
-app.use('/user/signup',require('./routes/webRoute'));
+app.use('/user',require('./routes/webRoute'));
+app.use('/admin',require('./controller/bread/bread'))
 
 
 
---
+++ app/controller/bread/bread.js
@@ -0,0 +1,75 @@
+const dbconfig = require("../../config/dbconfig");
+const mysql = require("mysql2/promise");
+const express = require("express");
+const router = express.Router();
+const upload = require('../../middleware/multerConfig')
+
+// Delete Card Function
+
+router.post("/delete/:id", async function (req, res) {
+  const { id } = req.params;
+  const connection = await mysql.createConnection(dbconfig);
+
+  try {
+    await connection.query("DELETE FROM cards WHERE event_id = ?", [id]);
+    await connection.end();
+    res.redirect("/");
+  } catch (error) {
+    console.log(`error in delete function`, error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Edit Card Function
+
+router.get("/edit/:id", async function (req, res) {
+  const { id } = req.params;
+  const connection = await mysql.createConnection(dbconfig);
+
+  try {
+    const [rows] = await connection.query(
+      "SELECT * from cards where event_id = ?",
+      [id]
+    );
+    connection.end();
+
+    if (rows.length == 0) {
+      return res.status(404).send("Card not found");
+    } else {
+      res.render("edit", { card: rows[0] });
+    }
+  } catch (error) {
+    console.log(`error in Edit function`, error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Update Card Function
+
+router.post("/edit/:id",upload.single('img'), async function (req, res) {
+  const { id } = req.params;
+  const connection = await mysql.createConnection(dbconfig);
+
+  try {
+    const { title, description, date, time, location, available_seats , old_image } =
+      req.body;
+
+    let img = old_image;
+
+    if(req.file){
+      img = req.file.filename;
+    }
+
+    await connection.query(
+      "UPDATE cards SET title=?, description=?, location=?, date=?, time=?, available_seats=?,img=? WHERE event_id=?",
+      [title, description, location, date, time, available_seats, img , id]
+    );
+    res.redirect('/');
+    
+  } catch (error) {
+    console.log(`error in update function`, error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
